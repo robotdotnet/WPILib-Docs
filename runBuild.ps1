8d34c55d10e76f9ea34c4965f83150f230d7bb36
Thad House <thadhouse1@gmail.com>
2017-01-12 22:45:21 -0800

    Runs docfx and zips output from script

--- runBuild.ps1
+++ runBuild.ps1
@@ -5,13 +5,26 @@ if ($env:APPVEYOR)
 }
 
 if ($env:APPVEYOR) {
-    $localFolder = "$env:APPVEYOR_BUILD_FOLDER\repos"
+    $localFolder = "$env:APPVEYOR_BUILD_FOLDER"
 } else {
-    $localFolder = ".\repos"
+    $localFolder = "."
 }
 
-git clone https://github.com/robotdotnet/frc-opencvsharp "$localFolder\frc-opencvsharp" --depth=1
-git clone https://github.com/robotdotnet/cameraserver "$localFolder\cameraserver" --depth=1
-git clone https://github.com/robotdotnet/wpilib "$localFolder\wpilib" --depth=1
-git clone https://github.com/robotdotnet/networktables "$localFolder\networktables" --depth=1
-git clone https://github.com/robotdotnet/wpilib-ctre "$localFolder\wpilib-ctre" --depth=1
+git clone https://github.com/robotdotnet/frc-opencvsharp "$localFolder\repos\frc-opencvsharp" --depth=1
+git clone https://github.com/robotdotnet/cameraserver "$localFolder\repos\cameraserver" --depth=1
+git clone https://github.com/robotdotnet/wpilib "$localFolder\repos\wpilib" --depth=1
+git clone https://github.com/robotdotnet/networktables "$localFolder\repos\networktables" --depth=1
+git clone https://github.com/robotdotnet/wpilib-ctre "$localFolder\repos\wpilib-ctre" --depth=1
+
+docfx
+
+Remove-Item "$localFolder\site.zip"
+
+Add-Type -Assembly System.IO.Compression.FileSystem
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Optimal
+[System.IO.Compression.ZipFile]::CreateFromDirectory("$localFolder\_site\",
+    "$localFolder\site.zip", $compressionLevel, $false)
+
+if ($env:APPVEYOR) {
+  Get-ChildItem .\artifacts\*.nupkg | % { Push-AppveyorArtifact $_.FullName -FileName $_.Name }
+}
