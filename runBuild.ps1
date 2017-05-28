4880417e4e18348a2998e47047337f6fd590cb07
Thad House <thadhouse1@gmail.com>
2017-05-28 16:00:11 -0700

    Switches to manually grab docfx rather then chocolatey

--- runBuild.ps1
+++ runBuild.ps1
@@ -1,15 +1,29 @@
-if ($env:APPVEYOR)
-{
-  # Install docfx
-  cinst docfx
-  
-  if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
-}
-
 if ($env:APPVEYOR) {
     $localFolder = "$env:APPVEYOR_BUILD_FOLDER"
 } else {
     $localFolder = "."
+}
+
+if ($env:APPVEYOR)
+{
+
+    $compilerName = "compiler"
+  # Install docfx
+  (New-Object System.Net.WebClient).DownloadFile("https://github.com/dotnet/docfx/releases/download/v2.17.7/docfx.zip", "$localFolder\$compilerName.zip")
+# unzip everything
+Add-Type -assembly "system.io.compression.filesystem"
+
+function Unzip($file)
+{
+  [io.compression.zipfile]::ExtractToDirectory("$localFolder\$file.zip", "$localFolder\$file")
+}
+
+echo "Extracting Docfx"
+Unzip($compilerName)
+
+    $env:Path += ";$localFolder\compiler"
+  
+  if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 }
 
 git clone https://github.com/robotdotnet/frc-utilities "$localFolder\repos\frc-utilities" --depth=1
