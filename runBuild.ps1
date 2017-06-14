135af0ddf2297ce553ebd7d6ecb4dbef6f762f36
Thad House <ThadHouse@users.noreply.github.com>
2017-06-14 12:09:44 -0700

    Updates to new CSPROJ format (#3)
    * Removes old csproj files
    
    Debugging why metadata doesn't work
    
    * Switches to manually grab docfx rather then chocolatey
    
    * Enables RDP
    
    * Updates docfx version to hopefully fix metadata builds
    
    * Forces framework to net46
    
    * Another attempt
    
    * Moves directory to repos folder to restore
    
    * reenables rdp
    
    * Removes global.json
    
    Breaking things
    
    * Revert "reenables rdp"
    
    This reverts commit 82cb6541c61067876673a74656d53dcd1ac1e879.
    
    * Restores FRC utilities
    
    * Uses VS 2017 image
    
    * Adds cameraserver
    
    * Adds OpenCvSharp
    
    * Switches to net451
    
    * Restores all repos
    
    * Adds wpilib back

--- runBuild.ps1
+++ runBuild.ps1
@@ -1,32 +1,53 @@
-if ($env:APPVEYOR)
-{
-  # Install docfx
-  cinst docfx
-  
-  if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
-}
-
 if ($env:APPVEYOR) {
     $localFolder = "$env:APPVEYOR_BUILD_FOLDER"
 } else {
     $localFolder = "."
 }
 
+if ($env:APPVEYOR)
+{
+
+    $compilerName = "compiler"
+  # Install docfx
+  (New-Object System.Net.WebClient).DownloadFile("https://github.com/dotnet/docfx/releases/download/v2.18.3/docfx.zip", "$localFolder\$compilerName.zip")
+# unzip everything
+Add-Type -assembly "system.io.compression.filesystem"
+
+function Unzip($file)
+{
+  [io.compression.zipfile]::ExtractToDirectory("$localFolder\$file.zip", "$localFolder\$file")
+}
+
+echo "Extracting Docfx"
+Unzip($compilerName)
+
+    $env:Path += ";$localFolder\compiler"
+  
+  if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
+}
+
 git clone https://github.com/robotdotnet/frc-utilities "$localFolder\repos\frc-utilities" --depth=1
-git clone https://github.com/robotdotnet/frc-opencvsharp "$localFolder\repos\frc-opencvsharp" --depth=1
-git clone https://github.com/robotdotnet/cameraserver "$localFolder\repos\cameraserver" --depth=1
+git clone https://github.com/robotdotnet/networktables "$localFolder\repos\networktables" --depth=1
+git clone https://github.com/robotdotnet/wpilib-ctre "$localFolder\repos\wpilib-ctre" --depth=1
+git clone https://github.com/robotdotnet/CameraServer "$localFolder\repos\cameraserver" --depth=1
+git clone https://github.com/robotdotnet/FRC-OpenCvSharp "$localFolder\repos\frc-opencvsharp" --depth=1
+
+dotnet restore "$localFolder\repos\frc-opencvsharp"
+dotnet restore "$localFolder\repos\frc-utilities"
+dotnet restore "$localFolder\repos\networktables"
+dotnet restore "$localFolder\repos\cameraserver"
+dotnet restore "$localFolder\repos\wpilib-ctre"
+
 git clone https://github.com/robotdotnet/wpilib "$localFolder\repos\wpilib" --depth=1
 if ($env:APPVEYOR) {
     nuget restore "$localFolder\repos\wpilib\robotdotnet-wpilib.sln"
 } else {
     .\repos\wpilib\NuGet.exe restore .\repos\wpilib\robotdotnet-wpilib.sln
 }
-git clone https://github.com/robotdotnet/networktables "$localFolder\repos\networktables" --depth=1
-git clone https://github.com/robotdotnet/wpilib-ctre "$localFolder\repos\wpilib-ctre" --depth=1
 
-dotnet restore
-
-git checkout master
+if (!$env:APPVEYOR) {
+  git checkout master
+}
 
 docfx
 
