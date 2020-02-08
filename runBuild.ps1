45fa9e6b30b6070225d4464753d885fe007186c4
Thad House <thadhouse1@gmail.com>
2020-02-07 16:03:24 -0800

    Update docs to start building 2020 artifacts.

--- runBuild.ps1
+++ runBuild.ps1
@@ -9,7 +9,7 @@ if ($env:APPVEYOR)
 
     $compilerName = "compiler"
   # Install docfx
-  (New-Object System.Net.WebClient).DownloadFile("https://github.com/dotnet/docfx/releases/download/v2.19/docfx.zip", "$localFolder\$compilerName.zip")
+  (New-Object System.Net.WebClient).DownloadFile("https://github.com/dotnet/docfx/releases/download/v2.48.1/docfx.zip", "$localFolder\$compilerName.zip")
 # unzip everything
 Add-Type -assembly "system.io.compression.filesystem"
 
@@ -26,24 +26,8 @@ Unzip($compilerName)
   if($LastExitCode -ne 0) { $host.SetShouldExit($LastExitCode )  }
 }
 
-git clone https://github.com/robotdotnet/frc-utilities "$localFolder\repos\frc-utilities" --depth=1
-git clone https://github.com/robotdotnet/networktables "$localFolder\repos\networktables" --depth=1
-git clone https://github.com/robotdotnet/wpilib-ctre "$localFolder\repos\wpilib-ctre" --depth=1
-git clone https://github.com/robotdotnet/CameraServer "$localFolder\repos\cameraserver" --depth=1
-git clone https://github.com/robotdotnet/FRC-OpenCvSharp "$localFolder\repos\frc-opencvsharp" --depth=1
-
-dotnet restore "$localFolder\repos\frc-opencvsharp"
-dotnet restore "$localFolder\repos\frc-utilities"
-dotnet restore "$localFolder\repos\networktables"
-dotnet restore "$localFolder\repos\cameraserver"
-dotnet restore "$localFolder\repos\wpilib-ctre"
-
 git clone https://github.com/robotdotnet/wpilib "$localFolder\repos\wpilib" --depth=1
-if ($env:APPVEYOR) {
-    nuget restore "$localFolder\repos\wpilib\robotdotnet-wpilib.sln"
-} else {
-    .\repos\wpilib\NuGet.exe restore .\repos\wpilib\robotdotnet-wpilib.sln
-}
+dotnet restore "$localFolder\repos\wpilib"
 
 if (!$env:APPVEYOR) {
   git checkout master
